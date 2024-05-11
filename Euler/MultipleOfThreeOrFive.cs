ed05ce0c2455ad6ea25aaadd6b593ee8e0ae0b9e
Parth <kparth@hydropoint.com>
2024-05-11 12:24:14 +0530

    Sum of Even Fibonacci numbers and multiple of three or five

---
+++ Euler/MultipleOfThreeOrFive.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Euler
+{
+    /// <summary>
+    /// If we list all the natural numbers below that are multiples of or, we get and.The sum of these multiples is 
+    /// .Find the sum of all the multiples of or below.
+    /// </summary>
+    public static class MultipleOfThreeOrFive
+    {
+        public static int GetSum(int upperLimit= 1000)
+        {
+            int sum = 0;
+
+            for(int i = 0; i<upperLimit; i++)
+            {
+                if(i % 3 == 0 || i % 5 == 0)
+                {
+                    sum += i;
+                }
+            }
+            return sum;
+        }
+    }
+}
--- Euler/Program.cs
+++ Euler/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Sum of multiple of 3 or 5 under 1000: "+MultipleOfThreeOrFive.GetSum());
+            Console.WriteLine("Sum of even numbers in the Fibonacci sequence under the value : 4000000" + EvenFibonacciNumbers.GetSumOfEvenNumbers());
         }
     }
 }
