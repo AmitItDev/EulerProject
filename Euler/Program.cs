d69e0d24de4b26e2d13fc7dac3ea30f9bab0b15e
Parth <kparth@hydropoint.com>
2024-05-13 21:54:09 +0530

    3. Largest prime factor

--- Euler/Program.cs
+++ Euler/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Euler
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Euler
 {
     internal class Program
     {
@@ -6,6 +8,7 @@
         {
             Console.WriteLine("Sum of multiple of 3 or 5 under 1000: "+MultipleOfThreeOrFive.GetSum());
             Console.WriteLine("Sum of even numbers in the Fibonacci sequence under the value : 4000000" + EvenFibonacciNumbers.GetSumOfEvenNumbers());
+            Console.WriteLine("The largest prime factor of 600851475143 is: " + LargestPrimeFactor.GetForNumber(600851475143));
         }
     }
 }
